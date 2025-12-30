375092523279e164a0017022f7095159f7a063ba
06-JON-06 <06-JON-06@users.noreply.github.com>
2025-12-30 13:25:23 +0000

    deploy: 6b31513ad5b2ba33be2ae1ee67cafae3416a6f91

---
+++ assets/index-EUjbl1Ko.js
@@ -0,0 +1,7 @@
+(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&n(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();const c=document.getElementById("app");c.innerHTML=`
+  <main>
+    <h1>Hello Vite</h1>
+    <p>Welcome to your Vite app.</p>
+    <button id="btn">Click me</button>
+  </main>
+`;document.getElementById("btn").addEventListener("click",()=>{alert("Hello from Vite!")});console.log("Vite app started");
